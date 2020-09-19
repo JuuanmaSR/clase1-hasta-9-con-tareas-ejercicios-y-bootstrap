ae9bb1d2db7a64599a0e509967e8c094b60be670
JuuanmaSR <juan_rider_sp@hotmail.com>
2020-09-19 15:35:51 -0300

    problema de borde rojo para errores
    Intentando solucionar porque no me aparecen los bordes rojos cuando hay un campo con un error

---
+++ js-xmas-edition-master/js/main.js
@@ -0,0 +1,110 @@
+const $form = document.querySelector(`#carta-a-santa`);
+const nombre = $form.nombre.value;
+const ciudad = $form.ciudad.value;
+const comportamiento = $form.comportamiento.value;
+const descripcionRegalo = $form["descripcion-regalo"].value;
+console.log(nombre);
+console.log(ciudad);
+console.log(comportamiento);
+console.log(descripcionRegalo);
+
+
+function validarNombre(nombre) {
+    if (nombre.length === 0) {
+        return "Este campo debe tener al menos 1 caracter"
+
+    }
+    if (nombre.length >= 50) {
+        return "Este campo debe tener menos de 50 caracteres"
+    };
+
+    if (!/^[A-z]+$/i.test(nombre)) {
+        return `El campo nombre solo acepta letras`
+    } else {
+        return ``;
+    }
+
+
+};
+
+
+function validarCiudad(ciudad) {
+    if (ciudad.length === 0) {
+        return `El campo ciudad no debe estar vacio`
+    } else {
+        return ``;
+    }
+
+};
+
+
+function validarDescripcionRegalo(descripcionRegalo) {
+    if (descripcionRegalo.length === 0) {
+        return `El campo descripcion regalo debe tener al menos 1 caracter`;
+    };
+    if (descripcionRegalo.length >= 100) {
+        return `El campo descripcion regalo debe tener menos de 100 caracteres`;
+    };
+
+
+    if (!/^[A-z0-9 ]+$/i.test(descripcionRegalo)) {
+        return `El campo descripcion solo puede contener letras y numeros `
+    } else {
+        return ``;
+    };
+};
+
+
+function validarFormulario(event) {
+    const $form = document.querySelector(`#carta-a-santa`);
+    const nombre = $form.nombre.value;
+    const ciudad = $form.ciudad.value;
+    const descripcionRegalo = $form[`descripcion-regalo`].value;
+
+
+    const errorNombre = validarNombre(nombre);
+    const errorCiudad = validarCiudad(ciudad);
+    const errorDescripcionRegalo = validarDescripcionRegalo(descripcionRegalo);
+
+    const errores = {
+        nombre: errorNombre,
+        ciudad: errorCiudad,
+        descripcionRegalo: errorDescripcionRegalo
+
+    }
+    console.log(errores);
+
+    manejarErrores(errores);
+
+    event.preventDefault();
+}
+
+
+
+function manejarErrores(errores) {
+  
+    errorNombre = errores.nombre; //nombre
+    errorCiudad = errores.ciudad; //ciudad
+    errorDescripcionRegalo = errores.descripcionRegalo; //descripcion regalo
+
+    if (errorNombre) {
+        $form.nombre.ClassName = "error";
+    } else {
+        $form.nombre.ClassName = "";
+    };
+
+    if (errorCiudad) {
+        $form.ciudad.ClassName = "error";
+    } else {
+        $form.ciudad.ClassName = "";
+    };
+
+    if (errores.descripcionRegalo) {
+        $form[`descripcion-regalo`].ClassName = "error";
+    } else {
+        $form[`descripcion-regalo`].ClassName = "";
+    };
+   
+};
+
+$form.onsubmit = validarFormulario;
--- js-xmas-edition-master/js/pruebas.js
+++ js-xmas-edition-master/js/pruebas.js
@@ -1,15 +1,110 @@
-function probarValidarNombre() {
-  console.assert(
-      validarNombre('') === 'Este campo debe tener al menos 1 caracter',
-      'Validar nombre no validó que el nombre no sea vacío',
-  );
+const $form = document.querySelector(`#carta-a-santa`);
+const nombre = $form.nombre.value;
+const ciudad = $form.ciudad.value;
+const comportamiento = $form.comportamiento.value;
+const descripcionRegalo = $form["descripcion-regalo"].value;
+console.log(nombre);
+console.log(ciudad);
+console.log(comportamiento);
+console.log(descripcionRegalo);
 
-  console.assert(
-      validarNombre(
-          '111111111111111111111111111111111111111111111111111111111111111111111111111111111111111') ===
-      'Este campo debe tener menos de 50 caracteres',
-      'Validar nombre no validó que el nombre sea menor a 50 caracteres',
-  );
+
+function validarNombre(nombre) {
+    if (nombre.length === 0) {
+        return "Este campo debe tener al menos 1 caracter"
+
+    }
+    if (nombre.length >= 50) {
+        return "Este campo debe tener menos de 50 caracteres"
+    };
+
+    if (!/^[A-z]+$/i.test(nombre)) {
+        return `El campo nombre solo acepta letras`
+    } else {
+        return ``;
+    }
+
+
+};
+
+
+function validarCiudad(ciudad) {
+    if (ciudad.length === 0) {
+        return `El campo ciudad no debe estar vacio`
+    } else {
+        return ``;
+    }
+
+};
+
+
+function validarDescripcionRegalo(descripcionRegalo) {
+    if (descripcionRegalo.length === 0) {
+        return `El campo descripcion regalo debe tener al menos 1 caracter`;
+    };
+    if (descripcionRegalo.length >= 100) {
+        return `El campo descripcion regalo debe tener menos de 100 caracteres`;
+    };
+
+
+    if (!/^[A-z0-9 ]+$/i.test(descripcionRegalo)) {
+        return `El campo descripcion solo puede contener letras y numeros `
+    } else {
+        return ``;
+    };
+};
+
+
+function validarFormulario(event) {
+    const $form = document.querySelector(`#carta-a-santa`);
+    const nombre = $form.nombre.value;
+    const ciudad = $form.ciudad.value;
+    const descripcionRegalo = $form[`descripcion-regalo`].value;
+
+
+    const errorNombre = validarNombre(nombre);
+    const errorCiudad = validarCiudad(ciudad);
+    const errorDescripcionRegalo = validarDescripcionRegalo(descripcionRegalo);
+
+    const errores = {
+        nombre: errorNombre,
+        ciudad: errorCiudad,
+        descripcionRegalo: errorDescripcionRegalo
+
+    }
+    console.log(errores);
+
+    manejarErrores(errores);
+
+    event.preventDefault();
 }
 
-probarValidarNombre();
+
+
+function manejarErrores(errores) {
+  
+    errorNombre = errores.nombre; //nombre
+    errorCiudad = errores.ciudad; //ciudad
+    errorDescripcionRegalo = errores.descripcionRegalo; //descripcion regalo
+
+    if (errorNombre) {
+        $form.nombre.ClassName = "error";
+    } else {
+        $form.nombre.ClassName = "";
+    };
+
+    if (errorCiudad) {
+        $form.ciudad.ClassName = "error";
+    } else {
+        $form.ciudad.ClassName = "";
+    };
+
+    if (errores.descripcionRegalo) {
+        $form[`descripcion-regalo`].ClassName = "error";
+    } else {
+        $form[`descripcion-regalo`].ClassName = "";
+    };
+   
+};
+
+$form.onsubmit = validarFormulario;
