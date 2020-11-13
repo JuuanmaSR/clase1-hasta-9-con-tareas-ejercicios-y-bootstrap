19fbbb35d862189ac595ec897344580f75eba874
JuuanmaSR <juan_rider_sp@hotmail.com>
2020-11-13 14:33:48 -0300

    boton resetear!
    Se agrego un boton resetear para mas funcionalidad.

--- introduccion-a-js-master/tareas/clase-6/tarea-2-clase-6.js
+++ introduccion-a-js-master/tareas/clase-6/tarea-2-clase-6.js
@@ -1,33 +1,38 @@
 
 let indice = 0;
 document.querySelector(`#agregar`).onclick = function (event) {
-    
+
     agregarIntegrante(incrementarIndice());
     mostrarBotonCalcular();
+    mostrarBotonResetear();
     event.preventDefault();
 };
-function incrementarIndice(){
+function incrementarIndice() {
     indice++;
     return indice;
 };
 
-document.querySelector(`#quitar`).onclick = function (event){
+document.querySelector(`#quitar`).onclick = function (event) {
     quitarIntegrante();
-    
-    
+
+
     event.preventDefault();
 };
 
-document.querySelector(`#calcular`).onclick = function(event){
+document.querySelector(`#calcular`).onclick = function (event) {
     let salariosAnuales = obtenerSalarios();
-    mostrarSalarios(`mayor`,calcularMayorSalarioAnual(salariosAnuales));
-    mostrarSalarios(`menor`,calcularMenorSalarioAnual(salariosAnuales));
-    mostrarSalarios(`promedio`,calcularPromedioSalarioAnual(salariosAnuales));
+    mostrarSalarios(`mayor`, calcularMayorSalarioAnual(salariosAnuales));
+    mostrarSalarios(`menor`, calcularMenorSalarioAnual(salariosAnuales));
+    mostrarSalarios(`promedio`, calcularPromedioSalarioAnual(salariosAnuales));
     mostrarSalarios(`promediomensual`, calcularPromedioSalarioMensual(salariosAnuales));
     mostrarResultado();
-    
-    
+
+
     event.preventDefault();
+};
+
+document.querySelector(`#resetear`).onclick=function(){
+    resetear();
 }
 
 function agregarIntegrante(indice) {
@@ -49,37 +54,85 @@ function agregarIntegrante(indice) {
     $integrantes.appendChild($div)
 };
 
-function quitarIntegrante(){
+function quitarIntegrante() {
     const $integrante = document.querySelectorAll(`.integrantes`)
     const listaIntegrantes = []
-    for(let i = 0; i<$integrante.length;i++){
+    for (let i = 0; i < $integrante.length; i++) {
         listaIntegrantes.push($integrante[i])
     };
-    
-    listaIntegrantes[listaIntegrantes.length-1].remove();
+
+    listaIntegrantes[listaIntegrantes.length - 1].remove();
     indice--;
 };
+function quitarIntegrantes() {
+    const $integrantes = document.querySelectorAll(`.integrantes`);
+    const listaDeIntegrantes = [];
+    for (let i = 0; i < $integrantes.length; i++) {
+        listaDeIntegrantes.push($integrantes[i])
+    };
 
-function obtenerSalarios(){
+    for (let i = 0; i < listaDeIntegrantes.length; i++) {
+        listaDeIntegrantes[i].remove();
+    };
+};
+
+function obtenerSalarios() {
     let $integrantes = document.querySelectorAll(`.integrantes input`)
-    let $salariosAnuales =[] 
-    for(let i = 0;i<$integrantes.length;i++){
+    let $salariosAnuales = []
+    for (let i = 0; i < $integrantes.length; i++) {
         $salariosAnuales.push(Number($integrantes[i].value));
     };
     return $salariosAnuales;
 };
 
-function mostrarSalarios(tipo,valor){
+function mostrarSalarios(tipo, valor) {
     document.querySelector(`#${tipo}-salario`).textContent = valor;
 };
 
-function mostrarBotonCalcular(){
+
+
+function mostrarBotonCalcular() {
     let $botonCalcular = document.querySelector(`#calcular`);
     $botonCalcular.className = ``;
-}
+};
 
-function mostrarResultado(){
+
+
+function mostrarResultado() {
     let resultado = document.querySelector(`#analisis`);
     resultado.className = ``;
 };
 
+
+
+function mostrarBotonResetear() {
+    let $botonResetear = document.querySelector(`#resetear`);
+    $botonResetear.className = ``;
+}
+
+
+function ocultarResultado() {
+    let resultado = document.querySelector(`#analisis`);
+    resultado.className = `oculto`;
+};
+
+
+
+function ocultarBotonResetear() {
+    let $botonResetear = document.querySelector(`#resetear`);
+    $botonResetear.className = `oculto`
+};
+
+
+function ocultarBotonCalcular(){
+    let $botonCalcular = document.querySelector(`#calcular`);
+    $botonCalcular.className = `oculto`;
+};
+
+function resetear() {
+    quitarIntegrantes();
+    ocultarResultado()
+    ocultarBotonResetear();
+    ocultarBotonCalcular()
+    indice = 0;
+}
