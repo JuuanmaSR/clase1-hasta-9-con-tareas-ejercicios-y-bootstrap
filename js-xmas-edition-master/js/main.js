95cfe51251e9f9f521812eed21f2452912b0bd75
JuuanmaSR <juan_rider_sp@hotmail.com>
2020-11-21 16:14:41 -0300

    Tareas clase 8
    Mostrar errores con borde rojo y en una lista desordenada en la interface usario. redireccion hacia otra pagina con un tiempo de espera de 4 segundos.

--- js-xmas-edition-master/js/main.js
+++ js-xmas-edition-master/js/main.js
@@ -4,32 +4,120 @@ const ciudad = $form.ciudad.value;
 const comportamiento = $form.comportamiento.value;
 const descripcionRegalo = $form[`descripcion-regalo`].value;
 
+/*
 console.log(nombre);
 console.log(ciudad);
 console.log(comportamiento);
 console.log(descripcionRegalo);
+*/
 
-function validarNombre(nombre){
-    if(nombre.length === 0){
+function validarNombre(nombre) {
+    if (nombre.length === 0) {
         return `El campo nombre debe tener al menos 1 caracter`;
     };
-    if(nombre.length >= 50){
+
+    if (nombre.length >= 50) {
         return `El campo nombre debe tener menos de 50 caracteres`;
     };
-    
+
+    if (!/^[a-z ]+$/i.test(nombre)) {
+        return `El campo nombre solo debe tener letras`;
+    };
+
     return ``;
 };
 
-function validarCiudad(ciudad){
-
+function validarCiudad(ciudad) {
+    if (ciudad.length === 0) {
+        return `El campo ciudad debe tener al menos un elemento seleccionado`;
+    } else {
+        return ``;
+    };
 };
 
-function validarDescripcionRegalo(descripcionRegalo){
+function validarDescripcionRegalo(descripcionRegalo) {
+    if (descripcionRegalo.length === 0) {
+        return `El campo descripcion regalo no debe estar vacio`;
+    };
 
+    if (descripcionRegalo.length >= 100) {
+        return `El campo descripcion regalo debe tener menos de 100 caracteres`;
+    };
+
+    if (!/^[a-z0-9, ]+$/i.test(descripcionRegalo)) {
+        return `El campo descripcion regalo solo debe tener letras y numeros y comas`;
+    };
+
+    return ``
 };
 
+function validarFormulario(event) {
+    const $form = document.querySelector(`#carta-a-santa`);
+
+    const nombre = $form.nombre.value;
+    const ciudad = $form.ciudad.value;
+    const descripcionRegalo = $form["descripcion-regalo"].value;
+
+    const errorNombre = validarNombre(nombre);
+    const errorCiudad = validarCiudad(ciudad);
+    const errorDescripcionRegalo = validarDescripcionRegalo(descripcionRegalo);
+
+    const errores = {
+        nombre: errorNombre,
+        ciudad: errorCiudad,
+        "descripcion-regalo": errorDescripcionRegalo
+    };
+    borrarErroresAcumulados();
+
+    const esExito = manejarErrores(errores) === 0 ;
+    if(esExito){
+        $form.className = `oculto`;
+        document.querySelector(`#exito`).className = ``;
+        setTimeout(redireccionarAlistaDeDeseos,4000);
+    };
+
+    event.preventDefault();
+};
+
+function manejarErrores(errores) {
+
+    const keys = Object.keys(errores)
+    const $errores = document.querySelector(`#errores`);
+    let contadorDeErrores = 0;
+
+    keys.forEach(function (key) {
+
+        const error = errores[key];
+        if (error) {
+            contadorDeErrores++;
+            const $error = document.createElement(`li`);
+            $error.className = `errores-mostrados`;
+            $error.innerText = error;
+            $errores.appendChild($error);
 
 
+            $form[key].className = `error`;
+        } else {
+            $form[key].className = ``;
+        }
+
+    });
+
+
+    return contadorDeErrores;
+};
+function borrarErroresAcumulados() {
+    const $errores = document.querySelectorAll(`.errores-mostrados`);
+    $errores.forEach(function (error) {
+        error.remove();
+    });
+};
+$form.onsubmit = validarFormulario;
+
+function redireccionarAlistaDeDeseos(){
+
+    window.location.href = `wishlist.html`;
+};
 
 
 
@@ -137,7 +225,7 @@ function manejarErrores(errores) {
     const $errores = document.querySelector(`#errores`);
 
     keys.forEach(function (key) {
-        const error = errores[key]; 
+        const error = errores[key];
 
         if (error) {
             $form[key].className = `error`;
@@ -148,7 +236,7 @@ function manejarErrores(errores) {
             $errores.appendChild($error);
 
         } else {
-            
+
             $form[key].className = ``;
         }
     });
@@ -157,23 +245,23 @@ function manejarErrores(errores) {
 
     };
 
-    /*  
+    /*
       errorNombre = errores.nombre; //nombre
       errorCiudad = errores.ciudad; //ciudad
       errorDescripcionRegalo = errores.descripcionRegalo; //descripcion regalo
-  
+
       if (errorNombre) {
           $form.nombre.className = "error";
       } else {
           $form.nombre.className = "";
       };
-  
+
       if (errorCiudad) {
           $form.ciudad.className = "error";
       } else {
           $form.ciudad.className = "";
       };
-  
+
       if (errores.descripcionRegalo) {
           $form[`descripcion-regalo`].className = "error";
       } else {
--- js-xmas-edition-master/js/pruebas.js
+++ js-xmas-edition-master/js/pruebas.js
@@ -18,17 +18,56 @@ function probarValidarNombre() {
         `Validar nombre no funciono con un string correcto`
     );
 
+    console.assert(
+        validarNombre(`123456`) === `El campo nombre solo debe tener letras`,
+        `Validar nombre no funciono con un string de numeros`
+    );
    
 };
 probarValidarNombre();
 
 function probarValidarCiudad(){
+    console.assert(
+        validarCiudad(``) === 
+        `El campo ciudad debe tener al menos un elemento seleccionado`,
+        `Validar ciudad no funciono con un campo vacio`
+    );
+    console.assert(
+        validarCiudad(`Buenos Aires`) === 
+        ``,
+        `Validar ciudad no funciono con un string correcto`
+    );
 
 };
+probarValidarCiudad();
 
 function probarValidarDescripcionRegalo(){
+    console.assert(
+        validarDescripcionRegalo(``) ===
+        `El campo descripcion regalo no debe estar vacio`,
+        `Validar descripcion regalo no funciono con un string vacio `
+    );
+
+    console.assert(
+        validarDescripcionRegalo(`aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+        aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+        aaaaaaaaaaaaaaaa`) ===
+        `El campo descripcion regalo debe tener menos de 100 caracteres`,
+        `Validar descripcion regalo no funciono con un string mayor a 100 caracteres`
+    );
+
+    console.assert(
+        validarDescripcionRegalo(`Quiero un auto de juguete`) ===
+        ``,
+        `Validar descripcion regalo no funciono con un string correcto`
+    );
+
+    console.assert(
+        validarDescripcionRegalo(`!&%&/()=?%&()=`) === `El campo descripcion regalo solo debe tener letras y numeros y comas`,
+        `Validar descripcion regalo no funciono con un string incorrecto de simbolos`
+    );
 
 };
-
+probarValidarDescripcionRegalo();
 
 
