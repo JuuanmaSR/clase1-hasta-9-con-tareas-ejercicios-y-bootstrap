426e6648a01cf24fc95acabe9a9b6142c7c0785a
JuuanmaSR <juan_rider_sp@hotmail.com>
2020-11-28 18:03:53 -0300

    Creacion de funciones manejar errores y validar primer formulario

--- js-xmas-edition-master/tarea/validar-tarea-clase6/validaciones-tarea-1.js
+++ js-xmas-edition-master/tarea/validar-tarea-clase6/validaciones-tarea-1.js
@@ -1,20 +1,51 @@
-const $form = document.querySelector(`#calculador-de-edades-por-integrante`);
-const $cantidadIntegrantes = $form[`cantidad-integrantes`].value;
+const $formPrimerPaso = document.querySelector(`#ingreso-cantidad-integrantes`);
 
 
 
 
 function validarCantidadIntegrantes($cantidadIntegrantes) {
+
     if ($cantidadIntegrantes === 0) {
-        return `El campo cantidad de integrantes no puede estar vacio`
-    };
+        return `El campo cantidad de integrantes no puede valer cero`
+    }
 
     if (!/^[0-9]+$/.test($cantidadIntegrantes)) {
         return `El campo cantidad de integrantes no puede tener decimales`
     };
-
     return ``;
 
 };
 
+function validarPrimerFormulario() {
+    let $cantidadIntegrantes = $formPrimerPaso[`cantidad-integrantes`].value;
+    let cantidadIntegrantes = Number($cantidadIntegrantes);
+
+    const errorCantidadIntegrantes = validarCantidadIntegrantes(cantidadIntegrantes);
+
+    const errores = {
+        "cantidad-integrantes": errorCantidadIntegrantes
+    };
+
+    manejarErrores(errores);
+
+
+};
+
+
+function manejarErrores(errores) {
+    const keys = Object.keys(errores);
+
+    keys.forEach(function (key) {
+        const error = errores[key]
+        if (error) {
+            $formPrimerPaso[key].className = `error`;
+
+        } else {
+            $formPrimerPaso[key].className = ``;
+        }
+
+        console.log(errores[key]);
+    });
+
+};
 
