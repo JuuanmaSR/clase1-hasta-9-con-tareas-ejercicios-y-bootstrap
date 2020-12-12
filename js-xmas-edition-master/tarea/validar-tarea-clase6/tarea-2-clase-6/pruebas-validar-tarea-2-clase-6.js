f39bab404f2c31852a522ed68a923a3d7d19465d
JuuanmaSR <juan_rider_sp@hotmail.com>
2020-12-12 19:25:32 -0300

    Probando las validaciones

--- js-xmas-edition-master/tarea/validar-tarea-clase6/tarea-2-clase-6/pruebas-validar-tarea-2-clase-6.js
+++ js-xmas-edition-master/tarea/validar-tarea-clase6/tarea-2-clase-6/pruebas-validar-tarea-2-clase-6.js
@@ -1,1 +1,26 @@
+function probarValidarSalariosAnualesEnNegativos() {
+    console.assert(
+        validarSalariosAnualesEnNegativos(0) === ``,
+        `Validar salarios anuales no funciono sin errores de numeros negativos`
+    );
+    console.assert(
+        validarSalariosAnualesEnNegativos(1) === `El campo salario anual no puede contener numeros negativos`,
+        `Validar salarios anuales no funciono con un error de numero negativo`
+    );
+
+};
+
+function probarValidarSalarioAnualMalFormulado(){
+    console.assert(
+        validarSalarioMalFormulados(0)=== ``,
+        `Validar salarios anuales no funciono sin errores en errores mal formulados`
+    );
+
+    console.assert(
+        validarSalarioMalFormulados(1) === `El campo salario anual esta mal formulado y solo puede contener numeros`,
+        `Validar salarios anaules no funciono con errrores en errores mal formulados`
+    );
+};
+probarValidarSalarioAnualMalFormulado();
+probarValidarSalariosAnualesEnNegativos();
 
