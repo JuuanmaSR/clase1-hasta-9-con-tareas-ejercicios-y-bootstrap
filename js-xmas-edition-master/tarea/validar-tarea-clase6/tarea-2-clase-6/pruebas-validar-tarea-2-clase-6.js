d28a59fd42d2e0518b4045dd76291ca47f54082a
JuuanmaSR <juan_rider_sp@hotmail.com>
2020-12-14 15:14:54 -0300

    Validando salarios vacios y sus pruebas
    Agregando la validacion hacia los inputs con valores vacios tipo string "", no tipo number ej: 0 , el 0 es aceptado como un salario sin ingresos .

--- js-xmas-edition-master/tarea/validar-tarea-clase6/tarea-2-clase-6/pruebas-validar-tarea-2-clase-6.js
+++ js-xmas-edition-master/tarea/validar-tarea-clase6/tarea-2-clase-6/pruebas-validar-tarea-2-clase-6.js
@@ -10,17 +10,25 @@ function probarValidarSalariosAnualesEnNegativos() {
 
 };
 
-function probarValidarSalarioAnualMalFormulado(){
+function probarValidarSalarioAnualMalFormulado() {
     console.assert(
-        validarSalarioMalFormulados(0)=== ``,
+        validarSalarioMalFormulados(0) === ``,
         `Validar salarios anuales no funciono sin errores en errores mal formulados`
     );
 
     console.assert(
         validarSalarioMalFormulados(1) === `El campo salario anual esta mal formulado y solo puede contener numeros`,
-        `Validar salarios anaules no funciono con errrores en errores mal formulados`
+        `Validar salarios anaules no funciono con errores de salarios mal formulados`
     );
 };
+
+function probarValidarSalariosVacios() {
+    validarSalariosVacios(0) === ``,
+    `Validar salarios anuales no funciono sin errores de inputs vacios`
+    validarSalariosVacios(1) === `El campo salario anual no puede estar vacio, pero puede ser cero`,
+    `Validar salarios anuales no funciono con errores de inputs vacios `
+};
+
 probarValidarSalarioAnualMalFormulado();
 probarValidarSalariosAnualesEnNegativos();
-
+probarValidarSalariosVacios();
